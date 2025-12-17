7012a9fad270fdb1840d6757f0fcdfea2f153083
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-17 12:40:57 +0500

    ch6_p4-5end. Added Marten. Implementing saving product via creation. Configuration marten with connection string postgresql

--- eshop-microservices-app/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices-app/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 // services
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly); 
-});
+builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
+builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); })
+    .UseLightweightSessions();
 var app = builder.Build();
 
 // pipelines
