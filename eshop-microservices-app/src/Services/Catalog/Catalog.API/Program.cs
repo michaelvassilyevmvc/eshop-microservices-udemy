d3ea498265417eaee8d42b498a741d17315ae2d2
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-18 15:35:46 +0500

    ch7_p10-11end. Impl. Custom Exception Handler

--- eshop-microservices-app/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices-app/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,3 @@
-using BuildingBlocks.Behaviors;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 var assymbly = typeof(Program).Assembly;
 
@@ -15,35 +11,16 @@ builder.Services.AddValidatorsFromAssembly(assymbly);
 builder.Services.AddCarter();
 builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); })
     .UseLightweightSessions();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // pipelines
 app.MapCarter();
 
-app.UseExceptionHandler(exceptionHandlerApp =>
+app.UseExceptionHandler(options =>
 {
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()
-            ?.Error;
-        if (exception == null)
-        {
-            return;
-        }
-
-        var problemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-        
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
+    
 });
+
 app.Run();
