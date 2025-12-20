202a811e074506d2faf04113a3c815b643167443
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-20 20:37:29 +0500

    ch11_p14_end. Добавление пакета Mapster. Реализация GetDiscount

--- eshop-microservices-app/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ eshop-microservices-app/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,13 +1,34 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services;
 
-public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        // TODO: GetDiscount from Database
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+        if (coupon is null)
+        {
+            coupon = new Coupon
+            {
+                ProductName = "No Discount",
+                Amount = 0,
+                Description = "No Discount Desc"
+            };
+        }
+
+        logger.LogInformation("Discount is retrieved for ProductName: {productName}, Amound: {amount}",
+            coupon.ProductName, coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
     }
 
     public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
@@ -20,7 +41,8 @@ public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
         return base.UpdateDiscount(request, context);
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,
+        ServerCallContext context)
     {
         return base.DeleteDiscount(request, context);
     }
