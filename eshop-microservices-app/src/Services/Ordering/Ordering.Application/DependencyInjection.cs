10f1aa7777749d60b795fe65cc32db44afee1752
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-23 12:29:28 +0500

    ch16_p15_end. Add to DI validation and logging behaviors for mediatr handler

--- eshop-microservices-app/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ eshop-microservices-app/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using BuildingBlocks.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
@@ -7,7 +8,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()); });
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
         return services;
     }
 }
