37acb5a79230675d0f5b882b67f8cc3defdc9284
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-23 12:22:12 +0500

    ch16_p13-14_end. Implemented DeleteOrder, UpdateOrder Handlers

---
+++ eshop-microservices-app/src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public record UpdateOrderCommand(OrderDto Order) : ICommand<UpdateOrderResult>;
+
+public record UpdateOrderResult(bool IsSuccess);
+
+public class UpdateOrderValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderValidator()
+    {
+        RuleFor(x => x.Order.Id)
+            .NotEmpty()
+            .WithMessage("Id is required.");
+        RuleFor(x => x.Order.OrderName)
+            .NotEmpty()
+            .WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId)
+            .NotNull()
+            .WithMessage("CustomerId is required");
+    }
+}
