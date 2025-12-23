7e7ec4dbb4cc770486760542c99539844b570ac3
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-23 13:02:12 +0500

    ch16_p16-17_end. Configured classes for handling domain events when creating and modifying an order

--- eshop-microservices-app/src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ eshop-microservices-app/src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -5,3 +5,6 @@ global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using FluentValidation;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
