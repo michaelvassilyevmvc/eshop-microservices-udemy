afccd1258168c948145b043da127ec983dc8d499
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-28 20:56:00 +0500

    ch20_p11-13. Add Refit-based ICatalogService and IBasketService clients. Configure GatewayAddress in appsettings.json. Install Refit.HttpClientFactory package.

--- eshop-microservices-app/src/WebApps/Shopping.Web/GlobalUsing.cs
+++ eshop-microservices-app/src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,1 +1,7 @@
 ﻿global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Models.Ordering;
+global using Refit;
+global using Shopping.Web.Services;
+
+
