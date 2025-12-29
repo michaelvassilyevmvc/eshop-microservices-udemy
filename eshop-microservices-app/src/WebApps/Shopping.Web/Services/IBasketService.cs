3232dcbb9864b4ed1d8e19cb4f97df32a7415bd4
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-29 13:02:26 +0500

    ch20_p29end. Fix typo in `IBasketService` CheckoutBasket route. Add placeholder files for discount database in Discount.Grpc service.

--- eshop-microservices-app/src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ eshop-microservices-app/src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -13,7 +13,7 @@ public interface IBasketService
     [Delete("/basket-service/basket/{userName}")]
     Task<DeleteBasketResponse> DeleteBasket(string userName);
 
-    [Post("/basekt-service/basket/checkout")]
+    [Post("/basket-service/basket/checkout")]
     Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
     
     public async Task<ShoppingCartModel> LoadUserBasket()
